6961dedc89c3b4eda4d2c548fbc165feac4c1457
PatrikPercinic <patrikrab123@gmail.com>
2024-11-20 01:55:41 +0100

    Added a config file for api server adress and endpoints

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const config = {
+  API_BASE_URL: 'https://localhost:7094/api',
+  ENDPOINTS: {
+    FLIGHTS_SEARCH: '/Flights/search',
+    IATA_CODES: '/Flights/iata'
+  }
+};
+
+export default config; 
